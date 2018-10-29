3c17aa74fd000df9aa3e0b31a0e02f8c5ae27972
BDMLab <bdmlab@Annikas-iMac.local>
2018-10-29 19:30:54 +0000

    Add drift simulations

--- glam/.ipynb_checkpoints/driftSim-checkpoint.py
+++ glam/.ipynb_checkpoints/driftSim-checkpoint.py
@@ -7,34 +7,32 @@ def simulate_trial_drift(parameters, values, gaze, boundary=1, error_weight=0.05
     v, gamma, s, tau, t0 = parameters
     n_items = len(values)
     # we consider ´s´ paramter as the standard deviation. 
-    if np.random.uniform(0, 1) < error_weight:
-        rt = int(np.random.uniform(*error_range))
-        choice = np.random.choice(n_items)
+    #if np.random.uniform(0, 1) < error_weight:
+    #    rt = int(np.random.uniform(*error_range))
+    #    choice = np.random.choice(n_items)
 
-    else:
-        drifts = expdrift(v, tau, gamma, values, gaze)
-        # initialize evidence accumlator for each item 
-        # only 2 items case
-        Evi0 = np.zeros(1) + t0 
-        Evi1 = np.zeros(1) + t0
-        time = 0
-        for i in range(n_items):
-            while (Evi0[time] < boundary) or (Evi0[time] < boundary):
+    drifts = expdrift(v, tau, gamma, values, gaze)
+    # initialize evidence accumlator for each item 
+    # only 2 items case
+    Evi0 = np.zeros(1) + t0 
+    Evi1 = np.zeros(1) + t0
+    time = 0
+    while (Evi0[time] < boundary) and (Evi1[time] < boundary):
+        
+            #mu = boundary / drifts[i]
+            #lam = (boundary / s)**2
+            Evi0_new = Evi0[time] +  drifts[0] +  np.random.normal(0,s)
+            Evi1_new = Evi1[time] +  drifts[1] +  np.random.normal(0,s)
             
-                #mu = boundary / drifts[i]
-                #lam = (boundary / s)**2
-                Evi0_new = Evi0[time] +  drift[0] +  np.random.normal(0,s)
-                Evi1_new = Evi1[time] +  drift[1] +  np.random.normal(0,s)
-                
-                Evi0 = np.append(Evi0,Evi0_new)
-                Evi1 = np.append(Evi1,Evi1_new)
-                
-                time += 1
+            Evi0 = np.append(Evi0,Evi0_new)
+            Evi1 = np.append(Evi1,Evi1_new)
+            
+            time += 1
 
-        choice = np.argmax([Evi0[-1],Evi1[-1]])
-        rt = int(np.round(time + t0))
-
-    return Evi0, Evi1
+    choice = np.argmax([Evi0[-1],Evi1[-1]])
+    rt = int(np.round(time + t0))
+    
+    return Evi0, Evi1, choice, rt
 
 
 def expdrift(v, tau, gamma, values, gaze):
