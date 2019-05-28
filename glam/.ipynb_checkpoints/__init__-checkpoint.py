892c2730256788b4376a5c9d475dc0e91283a39c
Pradyumna <pradyumna@AHBM-PRADYUMNA.local>
2019-05-28 22:01:51 +0100

    new figures

--- glam/.ipynb_checkpoints/__init__-checkpoint.py
+++ glam/.ipynb_checkpoints/__init__-checkpoint.py
@@ -4,6 +4,8 @@ from . import simulation
 from . import driftSim
 from . import plots_pretty
 from . import plots_pretty_aDDM
+from . import plots_pretty_GLAM
+
 from .models import make_models, GLAM
 from .fit import fit_models
 from .plots import plot_fit
