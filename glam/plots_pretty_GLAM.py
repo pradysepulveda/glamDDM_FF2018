6fc216fd13bd27d784a52cd97ae1cd7e4e59ca38
Pradyumna <pradyumna@AHBM-PRADYUMNA.local>
2020-02-18 16:45:43 +0000

    new plots

--- glam/plots_pretty_GLAM.py
+++ glam/plots_pretty_GLAM.py
@@ -27,11 +27,18 @@ def plot_fit(data, predictions, color_data = '#4F6A9A',label1 = 'More',label2 = 
    #     ax.text(-0.15, 1.175, label, transform=ax.transAxes,
    #             fontsize=16, fontweight='bold', va='top')
 
-
+    fsize = 30
+    
+    for axis1 in [axs[0][0],axs[0][1],axs[1][0],axs[1][1]]:
+        axis1.xaxis.label.set_fontsize(fontsize = fsize) # x label
+        axis1.yaxis.label.set_fontsize(fontsize = fsize) # Y label
+        axis1.tick_params(axis="x", labelsize=20)
+        axis1.tick_params(axis="y", labelsize=20)
+    
     patch1 = mpatches.Patch(facecolor=color_data,hatch=r'', label = label1)
     patch2 = mpatches.Patch(facecolor='#606060',hatch=r'', label = label2)
 
-    leg = plt.legend(handles=[patch1,patch2],fontsize=14,loc = 'lower right')
+    leg = plt.legend(handles=[patch1,patch2],fontsize=25,loc = 'lower right')
     leg.get_frame().set_facecolor('none')
     leg.get_frame().set_linewidth(0.0)
 
@@ -147,7 +154,7 @@ def plot_rt_by_difficulty(data, predictions=None, ax=None, xlims=(1.5, 8.5), xla
             ax.plot(x, means, '-o', markerfacecolor=c_pred[i],color=c_pred[i], linewidth=2.5, markersize = 10)
 
     #ax.set_ylim(0, 5000)
-    ax.set_xlabel('|$ΔVal_{Bins}$|')
+    ax.set_xlabel('|$ΔValue_{Bins}$|')
     ax.set_ylabel('RT (ms)')
     ax.set_xticks(x[::xlabel_skip])
     ax.set_xticklabels(np.around(means.index.values[::xlabel_skip],decimals = 1))
@@ -241,7 +248,7 @@ def plot_rt_by_difficulty_zSc(data, predictions=None, ax=None, xlims=(1.5, 8.5),
             ax.plot(x, means, '-o', markerfacecolor=c_pred[i],color=c_pred[i], linewidth=2.5, markersize = 10)
 
     #ax.set_ylim(2000, 3500)
-    ax.set_xlabel('|$ΔVal_{Bins}$|')
+    ax.set_xlabel('|$ΔValue_{Bins}$|')
     ax.set_ylabel('zRT (ms)')
     ax.set_xticks(x[::xlabel_skip])
     ax.set_xticklabels(np.around(means.index.values[::xlabel_skip],decimals = 1))
